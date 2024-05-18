a9c3ca8f71eca0ebb1edb3c1a34d3a2a34e0cc16
Belinglow <belindakyanet@gmail.com>
2024-05-18 15:24:39 +0300

    command line arguments

--- main.cpp
+++ main.cpp
@@ -9,6 +9,30 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 std::string directory, password_file; 
-	int port = 0;													
+	int port = 0;
+for (int i = 1; i < argc; i += 2)
+	{														
+		std::string arg = argv[i]; x i
+		if (arg == "-d")
+		{													 
+			directory = argv[i + 1]; 
+		}
+		else if (arg == "-p")
+		{																
+			port = std::stoi(argv[i + 1]); 
+		}
+		else if (arg == "-u")
+		{															  
+			password_file = argv[i + 1]; 
+		}
+	}
+
+
+
+
+
+
+
+
     return 0;
 }
