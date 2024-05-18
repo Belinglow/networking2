8e274e7c04649fbf0ddb7487199f0776aea4f703
Belinglow <belindakyanet@gmail.com>
2024-05-18 15:15:47 +0300

    Getting arguments from the user command

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
-int main()
+int main(int argc, char *argv[])
 {
+	// Check if the correct number of arguments is provided
+	if (argc != 7)
+	{
+		std::cerr << "Usage: " << argv[0] << " -d directory -p port -u password_file\n";
+		return 1;
+	}
+
     return 0;
 }
